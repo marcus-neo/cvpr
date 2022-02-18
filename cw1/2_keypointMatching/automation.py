7beaf94ace52996b06e515f12f77ff8211b6127c
marcus418 <56596363+marcus-neo@users.noreply.github.com>
2022-02-18 14:29:24 +0000

    updated code, removed images

--- cw1/2_keypointMatching/automation.py
+++ cw1/2_keypointMatching/automation.py
@@ -1,9 +1,7 @@
 from time import time
 from typing import Tuple, Union
-from cv2 import CV_8SC2
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
 MIN_MATCH_COUNT = 10
 
@@ -12,8 +10,6 @@ def correspondences_automation(
     img1: np.ndarray, img2: np.ndarray
 ) -> Tuple[
     bool,
-    Union[np.ndarray, None],
-    Union[np.ndarray, None],
     Union[np.ndarray, None],
     Union[np.ndarray, None],
 ]:
@@ -27,7 +23,6 @@ def correspondences_automation(
         - The source points and destination points of good matches.
         - The source points and destination points of bad matches.
     """
-    print(img1)
     # Convert the images to grayscale
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)  # queryImage
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)  # trainImage
@@ -53,29 +48,18 @@ def correspondences_automation(
     # Store all the good matches as per Lowe's ratio test.
     # Also store all the bad matches, i.e. distance > 0.99
     good = []
-    bad = []
     for m, n in matches:
         if m.distance < 0.8 * n.distance:
             good.append(m)
-        elif m.distance > 0.99 * n.distance:
-            bad.append(m)
 
     if len(good) < MIN_MATCH_COUNT:
-        return False, None, None, None, None
+        return False, None, None
 
     # Store the source and destination points of the good matches
     src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
     dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
-    # Store the source and destination points of the bad matches
-    src_pts_bad = np.float32([kp1[m.queryIdx].pt for m in bad]).reshape(
-        -1, 1, 2
-    )
-    dst_pts_bad = np.float32([kp2[m.trainIdx].pt for m in bad]).reshape(
-        -1, 1, 2
-    )
-
-    return True, src_pts, dst_pts, src_pts_bad, dst_pts_bad
+    return True, src_pts, dst_pts
 
 
 if __name__ == "__main__":
@@ -84,10 +68,13 @@ if __name__ == "__main__":
     image_2 = "data/RIGHT_IMG.JPG"
     ogimg1 = cv2.imread(image_1)
     ogimg2 = cv2.imread(image_2)
-    ret, src, dst, src_bad, dst_bad = correspondences_automation(
+    ret, src, dst = correspondences_automation(
         ogimg1, ogimg2
     )
     end_time = time()
-    # print(src, dst, src_bad, dst_bad)
-    print(len(src))
-    print("time:", end_time-start_time)
+    time_taken = end_time - start_time
+    good_points = len(src)
+    rate = good_points / time_taken
+    print("good points", good_points)
+    print("time:", time_taken)
+    print("points per second," rate)
